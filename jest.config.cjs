6877b83aa0bbc1808c06b0155f9901a026fc9b51
nickybondarinno <nickybondarinno@gmail.com>
2025-05-22 19:02:15 +0300

    feat: add SVG mock and update Jest configuration for module name mapping

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js'
+  },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
