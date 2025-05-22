dedd85994750dbfacfec41de24932dd661bc09eb
nickybondarinno <nickybondarinno@gmail.com>
2025-05-22 19:03:56 +0300

    feat: add style mock for CSS files in Jest configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.svg$': '<rootDir>/__mocks__/svgMock.js'
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    '\\.css$': '<rootDir>/__mocks__/styleMock.js'
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
